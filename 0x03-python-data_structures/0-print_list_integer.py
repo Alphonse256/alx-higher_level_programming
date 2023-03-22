abd2a764954366f0b82001828d57ec153fece6df
Alphonse256 <doctorfipaca241@gmail.com>
2023-03-21 18:53:29 -0700

    Printed list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# print_list_integers - Prints the list of integers each on a newline
+# @my_lists: Stores the list of integers
+# Return: Always 0 on success
+
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print(i)
