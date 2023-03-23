dac0433b098ffd9f555b714ec28268ee9edb5233
Alphonse256 <doctorfipaca241@gmail.com>
2023-03-23 04:04:00 -0700

    Retrieved an item at a specified index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 # element_at - Prints a number at a specified index
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0:
-        return None
-    if idx > length:
+    length = len(my_list) - 1
+    if idx < 0 or idx > length:
         return None
     else:
-        return my_list[idx]
+        return (my_list[idx])
