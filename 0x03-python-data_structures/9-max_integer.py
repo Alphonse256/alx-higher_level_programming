5c763a35b45a6334002c839f6858609a9eeca74c
Alphonse256 <doctorfipaca241@gmail.com>
2023-05-02 07:08:02 -0700

    Returned the maximum integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# max_integer - Finds the largest number in a list
+
+def max_integer(my_list=[]):
+    max_number = my_list[0]
+    for num in my_list:
+        if num > max_number:
+            max_number = num
+    return (max_number)
