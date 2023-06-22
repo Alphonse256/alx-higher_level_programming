d6b1d03bda0496e6f653e089a9eab4ed55c73aae
Alphonse256 <doctorfipaca241@gmail.com>
2023-06-22 03:19:07 -0700

    Looped through an array of strings

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-let arrayStr = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i <= arrayStr.length; i++) {
-  console.log(arrayStr[i]);
+for (let string in ['C is fun', 'Python is cool', '"JavaScript is amazing']) {
+  console.log(string);
 }
