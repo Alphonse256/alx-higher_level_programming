2954f85898018ac9b327425e71f77ac5d8345edb
Alphonse256 <doctorfipaca241@gmail.com>
2023-06-26 00:09:36 -0700

    Looped through an array of strings

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (let string of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+for (const string of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(string);
 }
