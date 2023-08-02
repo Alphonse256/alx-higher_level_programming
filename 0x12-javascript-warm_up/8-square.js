e061fa088078cc11c537ee61ee6fcf10a21bb41b
Alphonse256 <doctorfipaca241@gmail.com>
2023-08-02 01:52:33 -0700

    Printed square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const x = 'X';
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     console.log(x.repeat(size));
   }
 }
