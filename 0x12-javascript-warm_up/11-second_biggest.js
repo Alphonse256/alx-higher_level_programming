5fa91510cb55c6c4d93bda4df63ab88d70a553ec
Alphonse256 <doctorfipaca241@gmail.com>
2023-08-02 03:03:41 -0700

    Print second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const arguments = process.argv.slice(2);
-const numbers = arguments.map(args => Number(args));
+const argumentList = process.argv.slice(2);
+const numbers = argumentList.map(args => Number(args));
 if (numbers.length < 2) {
   console.log('0');
 } else {
-    const sortedNumbers = numbers.sort((a, b) => b - a);
-    console.log(sortedNumbers[1]);
-  }
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
+}
