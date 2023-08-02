be031bca202a74859739d3cb73d80aa4456bb521
Alphonse256 <doctorfipaca241@gmail.com>
2023-08-02 01:44:45 -0700

    Printed square

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const arguments = parseInt(process.argv);
+if (process.argv.length < 2) {
+  console.log('0');
+} else {
+    console.log(arguments);
+  }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
+let x = 'X';
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-      console.log('X');
-	}
-  }
+    for (i = 0; i < size; i++) {
+      console.log(x.repeat(size));
+    }
 }
