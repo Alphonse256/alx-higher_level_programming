c7e4dde21e2330533ab406142b63470b177c3523
Alphonse256 <doctorfipaca241@gmail.com>
2023-08-02 02:10:26 -0700

    Print factorial of integer

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (isNaN(n)) {
-    console.log('1');
-  } else if (n <= 1) {
-      console.log('1');
-    } else {
-        console.log(n * factorial(n - 1));
-    }
+  if (isNaN(n) || n === 0){
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
